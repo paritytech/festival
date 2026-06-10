eac05378f522261658e30070e7aa5bac8dd4a840
Daria Mikhailova <daria@parity.io>
2026-06-10 15:04:39 +0200

    copy update

--- packages/attendee/app/pages/my/badges/welcome.vue
+++ packages/attendee/app/pages/my/badges/welcome.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 const steps = [
-  { illustration: '/onb-badge-3.svg', title: 'Find a community session' },
-  { illustration: '/onb-badge-4.svg', title: 'Tap Collect Badge and show your QR to the host' },
-  { illustration: '/onb-badge-5.svg', title: 'Get your unique badge, forever' },
+  { illustration: '/onb-badge-3.svg', title: 'Find & attend a community session' },
+  { illustration: '/onb-badge-4.svg', title: 'Show your QR to collect the badge' },
+  { illustration: '/onb-badge-5.svg', title: 'Build your unique badge collection' },
 ]
 </script>
 
